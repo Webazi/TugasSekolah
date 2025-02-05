0570c44b5ba308be56d30812349026b5b58b6b54
Webazi <132550037+Webazi@users.noreply.github.com>
2025-02-05 12:01:38 +0000

    r

--- backend/Azy/config/config.js
+++ backend/Azy/config/config.js
@@ -1,8 +1,12 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize ('Azy','root','Aji190227*',{
-    host: 'localhost',
-    dialect:"mysql"
-});
-
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT || 3306,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 export default db;
